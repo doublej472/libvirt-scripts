3f4b39c86993f0493dd24bcc854bc1a362c0a200
Jonathan Frederick <doublej472@gmail.com>
2019-10-05 16:58:17 -0700

    Change vm pool

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 # Where your qcow2 vm images are located
 VMDIR=/data/vms
 # The storage pool that VMDIR points to
-VMPOOL=vms
+VMPOOL=vmstorage
 # The network bridge that your VMs are connected to
 VMBRIDGE=br0
 # The os-variant set when creating VMs
